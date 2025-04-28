8fbd51daa14a278f3510c16c13a1f666027f2b8c
Minju-1017 <mjk373737@gmail.com>
2025-04-28 17:51:07 +0900

    xdm index 수정

--- aa/src/main/java/com/aa/module/IndexController.java
+++ aa/src/main/java/com/aa/module/IndexController.java
@@ -1,25 +1,16 @@
 package com.aa.module;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.aa.module.product.ProductService;
 
 @Controller
 public class IndexController {
-	
-	@Autowired
-	ProductService productService;
 	
 	private String path_admin = "xdm/";
 	private String path_user = "usr/";
 	
 	@RequestMapping(value = "/xdm/index")	
-	public String xdmIndex(Model model) {
-		model.addAttribute("totalPrice",productService.totalPrice());
-		model.addAttribute("totalOrder",productService.totalOrder());
+	public String xdmIndex() {
 		return path_admin + "index";
 	}
 	
