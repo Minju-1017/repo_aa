9ee8004ec9c0f85cdf248485a7c4b82088af32f5
Minju-1017 <mjk373737@gmail.com>
2025-04-29 17:25:58 +0900

    메뉴 정리

--- aa/src/main/java/com/aa/Constants.java
+++ aa/src/main/java/com/aa/Constants.java
@@ -20,22 +20,7 @@ public class Constants {
 	// Login Session Time
 	public static int SESSION_MINUTE = 60 * 30; // 30분
 	
-	// Order에서 사용하는 코드 그룹
-	public final static int ORDERS_CODE_GROUP_SEQ_ORDER_STATE = 1; // oStateCd
-	public final static int ORDERSPRODUCT_CODE_GROUP_SEQ_ORDERPRODUCT_STATE = 7; // opStateCd
-	public final static int ORDERSDELIVERY_CODE_GROUP_SEQ_COMPANY = 8; // oDeliveryCompCd
-	
-	// oStateCd
-	public final static int ORDERS_CODE_SEQ_ORDER_STATE_NEW = 1;
-	public final static int ORDERS_CODE_SEQ_ORDER_STATE_READY_IN = 2;
-	public final static int ORDERS_CODE_SEQ_ORDER_STATE_READY_OUT = 3;
-	public final static int ORDERS_CODE_SEQ_ORDER_STATE_DELI = 4;
-	public final static int ORDERS_CODE_SEQ_ORDER_STATE_DELI_COMPLATE = 5;
-	public final static int ORDERS_CODE_SEQ_ORDER_STATE_END = 21;
-	
-	// opStateCd
-	public final static int ORDERS_CODE_SEQ_ORDER_PRODUCT_STATE_REQ_DELI = 6;
-	public final static int ORDERS_CODE_SEQ_ORDER_PRODUCT_STATE_V_ORDER = 7;
-	public final static int ORDERS_CODE_SEQ_ORDER_PRODUCT_STATE_IN = 8;
+	// Animal에서 사용하는 코드 그룹
+	public final static int USER_ANIMAL_CATECORY = 1; // uaCateCd
 	
 }
