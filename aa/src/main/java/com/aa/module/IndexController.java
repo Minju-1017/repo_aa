788efb59b4b661bf72f2fb970c94808016fe96f5
jty5606 <jty2298@gmail.com>
2025-05-07 17:54:30 +0900

    인덱스

--- aa/src/main/java/com/aa/module/IndexController.java
+++ aa/src/main/java/com/aa/module/IndexController.java
@@ -1,10 +1,20 @@
 package com.aa.module;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;import com.aa.module.miss.MissDto;
+import com.aa.module.miss.MissService;
+import com.aa.module.miss.MissVo;
 
 @Controller
 public class IndexController {
+	
+	@Autowired
+	MissService missService;
 	
 	private String path_admin = "xdm/";
 	private String path_user = "usr/";
@@ -15,8 +25,10 @@ public class IndexController {
 	}
 	
 	@RequestMapping(value = "/usr/index")	
-	public String usrIndex() {	
-		
+	public String usrIndex(Model model,@ModelAttribute("vo")MissVo vo) {	
+		List<MissDto> asdf = missService.mbList(vo);
+		System.out.println("#################" + asdf.size());
+		model.addAttribute("list",asdf);
 		return path_user + "index";
 	}
 }
